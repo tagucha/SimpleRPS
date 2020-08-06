f14062e1e381e8e554d4ec69665ca1b702be75ef
tagucha <tagucha.dev@gmail.com>
2020-08-06 18:25:40 +0900

    Base plugin

---
+++ src/tgcdev/sprs/PluginMain.java
@@ -0,0 +1,15 @@
+package tgcdev.sprs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginMain extends JavaPlugin {
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
